b4d4e46664b6989bd77bf6cad6aed05db5736c44
sinstranger <sinstranger@yandex.ru>
2024-03-20 09:25:50 +0300

    Clean comments

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -1,51 +1,40 @@
-const passport = require('passport');
-const UserModel = require('../../models/UserModel');
-const LocalStrategy = require('passport-local').Strategy;
-// const JwtStrategy = require('passport-jwt').Strategy;
-// const ExtractJwt = require('passport-jwt').ExtractJwt;
-// const opts = {};
-// opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-// opts.secretOrKey = process.env.JWT_SIGN_KEY || 'secret';
+const passport = require('passport')
+const UserModel = require('../../models/UserModel')
+const LocalStrategy = require('passport-local').Strategy
 
-passport.use('local', new LocalStrategy({ usernameField: 'email' }, async function (email, password, cb) {
-  try {
-    const user = await UserModel.findOne({ email: email });
-    
-    if (!user) {
-      return cb(null, false, { message: 'Неверный логин' });
-    }
+passport.use(
+	'local',
+	new LocalStrategy({ usernameField: 'email' }, async function (
+		email,
+		password,
+		cb
+	) {
+		try {
+			const user = await UserModel.findOne({ email: email })
 
-    const isPasswordValid = await user.comparePassword(password);
-    
-    if (!isPasswordValid) {
-      return cb(null, false, { message: 'Неверный пароль' });
-    }
+			if (!user) {
+				return cb(null, false, { message: 'Неверный логин' })
+			}
 
-    return cb(null, user);
-  } catch (err) {
-    return cb(err);
-  }
-}));
+			const isPasswordValid = await user.comparePassword(password)
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+			if (!isPasswordValid) {
+				return cb(null, false, { message: 'Неверный пароль' })
+			}
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+			return cb(null, user)
+		} catch (err) {
+			return cb(err)
+		}
+	})
+)
 
+passport.serializeUser(function (user, done) {
+	done(null, user)
+})
 
-// passport.use('jwt', new JwtStrategy(opts, function (jwt_payload, cb) {
-// 	UserModel.findById(jwt_payload.id, function (err, user) {
-// 		if (err) {
-// 			return cb(err);
-// 		}
-// 		if (!user) {
-// 			return cb(null, false, {message: 'Неверный токен'});
-// 		}
-// 		return cb(null, user);
-// 	});
-// }));
+passport.deserializeUser(function (user, done) {
+	done(null, user)
+})
 
-module.exports = passport;
+module.exports = passport
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,6 +1,6 @@
-const UserModel = require('../models/UserModel');
-const passport = require("../config/passport/passport");
-const jwt = require('jsonwebtoken');
+const UserModel = require('../models/UserModel')
+const passport = require('../config/passport/passport')
+const jwt = require('jsonwebtoken')
 
 const UserController = {
 	// Создание и сохранение нового пользователя
@@ -9,15 +9,17 @@ const UserController = {
 			// Проверка наличия обязательных полей
 			if (!req.body.login || !req.body.password || !req.body.email) {
 				return res.status(400).json({
-					message: 'Недостаточно данных для создания пользователя'
-				});
+					message: 'Недостаточно данных для создания пользователя',
+				})
 			}
 			// Проверка уникальности логина и email
-			const existingUser = await UserModel.findOne({$or: [{login: req.body.login}, {email: req.body.email}]});
+			const existingUser = await UserModel.findOne({
+				$or: [{ login: req.body.login }, { email: req.body.email }],
+			})
 			if (existingUser) {
 				return res.status(409).json({
-					message: 'Пользователь с таким логином или email уже существует'
-				});
+					message: 'Пользователь с таким логином или email уже существует',
+				})
 			}
 			// Создание нового пользователя
 			const user = new UserModel({
@@ -25,86 +27,95 @@ const UserController = {
 				password: req.body.password,
 				email: req.body.email,
 				firstName: req.body.firstName,
-				lastName: req.body.lastName
-			});
+				lastName: req.body.lastName,
+			})
 			// Сохранение пользователя в базе данных
-			await user.save();
+			await user.save()
 			res.status(201).json({
-				message: 'Пользователь успешно создан'
-			});
+				message: 'Пользователь успешно создан',
+			})
 		} catch (err) {
 			res.status(500).json({
-				message: err.message || 'Произошла ошибка при создании пользователя'
-			});
+				message: err.message || 'Произошла ошибка при создании пользователя',
+			})
 		}
 	},
 	// Аутентификация пользователя и генерация токена
 	loginUser: async (req, res, next) => {
-  //  return passport.authenticate('local', {
-  //   successRedirect: '/',
-  //   // failureRedirect: '/error',
-	// 	failureMessage: true
-  // }, (err,user) => {
-  //    console.log(err, user);
-  //    res.json(user);
-  //  })(req, res, next)
-
-		passport.authenticate('local', {session: false}, (err, user, info) => {
+		passport.authenticate('local', { session: false }, (err, user, info) => {
 			if (err || !user) {
 				return res.status(401).json({
-					message: info ? info.message : 'Неверный логин или пароль'
-				});
+					message: info ? info.message : 'Неверный логин или пароль',
+				})
 			}
 			// Генерация токена
-			const token = jwt.sign({id: user._id}, process.env.JWT_SIGN_KEY, {expiresIn: process.env.JWT_TOKEN_LIFETIME});
+			const token = jwt.sign({ id: user._id }, process.env.JWT_SIGN_KEY, {
+				expiresIn: process.env.JWT_TOKEN_LIFETIME,
+			})
 			// Отправка токена клиенту
 			res.status(200).json({
 				message: 'Пользователь успешно аутентифицирован',
-				token: token
-			});
-		})(req, res);
+				token: token,
+			})
+		})(req, res)
 	},
 	// Получение информации о текущем пользователе по токену
 	getUserInfo: async (req, res) => {
-		passport.authenticate('jwt', {session: false}, async (err, user, info) => {
-			if (err || !user) {
-				return res.status(401).json({
-					message: info ? info.message : 'Неверный токен'
-				});
+		passport.authenticate(
+			'jwt',
+			{ session: false },
+			async (err, user, info) => {
+				if (err || !user) {
+					return res.status(401).json({
+						message: info ? info.message : 'Неверный токен',
+					})
+				}
+				// Отправка информации о пользователе клиенту
+				res.status(200).json(user)
 			}
-			// Отправка информации о пользователе клиенту
-			res.status(200).json(user);
-		})(req, res);
+		)(req, res)
 	},
 	// Обновление информации о текущем пользователе по токену
 	updateUserInfo: async (req, res) => {
-		passport.authenticate('jwt', {session: false}, async (err, user, info) => {
-			if (err || !user) {
-				return res.status(401).json({
-					message: info ? info.message : 'Неверный токен'
-				});
+		passport.authenticate(
+			'jwt',
+			{ session: false },
+			async (err, user, info) => {
+				if (err || !user) {
+					return res.status(401).json({
+						message: info ? info.message : 'Неверный токен',
+					})
+				}
+				// Поиск и обновление пользователя по id из токена
+				const updatedUser = await UserModel.findByIdAndUpdate(
+					user._id,
+					req.body,
+					{ new: true, runValidators: true }
+				)
+				// Отправка обновленной информации о пользователе клиенту
+				res.status(200).json(updatedUser)
 			}
-			// Поиск и обновление пользователя по id из токена
-			const updatedUser = await UserModel.findByIdAndUpdate(user._id, req.body, {new: true, runValidators: true});
-			// Отправка обновленной информации о пользователе клиенту
-			res.status(200).json(updatedUser);
-		})(req, res);
+		)(req, res)
 	},
 	// Удаление текущего пользователя по токену
 	deleteUser: async (req, res) => {
-		passport.authenticate('jwt', {session: false}, async (err, user, info) => {
-			if (err || !user) {
-				return res.status(401).json({
-					message: info ? info.message : 'Неверный токен'
-				});
+		passport.authenticate(
+			'jwt',
+			{ session: false },
+			async (err, user, info) => {
+				if (err || !user) {
+					return res.status(401).json({
+						message: info ? info.message : 'Неверный токен',
+					})
+				}
+				// Поиск и удаление пользователя по id из токена
+				await UserModel.findByIdAndDelete(user._id)
+				// Отправка сообщения об успешном удалении пользователя клиенту
+				res.status(200).json({
+					message: 'Пользователь успешно удален',
+				})
 			}
-			// Поиск и удаление пользователя по id из токена
-			await UserModel.findByIdAndDelete(user._id);
-			// Отправка сообщения об успешном удалении пользователя клиенту
-			res.status(200).json({
-				message: 'Пользователь успешно удален'
-			});
-		})(req, res);
-	}
-};
-module.exports = UserController;
+		)(req, res)
+	},
+}
+module.exports = UserController
